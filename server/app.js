cc6abea46e6ecc5265374838a403ab281e6a98f6
Joe Yan <chengmu.yan@gmail.com>
2020-04-22 19:35:09 -0400

    set up static file serving

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const socketIO = require('socket.io');
@@ -7,11 +8,14 @@ const app = express();
 const httpServer = createServer(app);
 const io = socketIO(httpServer);
 
+console.log(path.join(__dirname, '../client/build/static'));
+app.use(express.static(path.join(__dirname, '../client/build/')));
+
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
 
 io.on('connection', () => {
   console.log('socket connected');
