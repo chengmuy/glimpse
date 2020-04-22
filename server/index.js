e6834c4dd224655d1070903e53a3fc0eedd94c47
Joe Yan <chengmu.yan@gmail.com>
2020-04-22 16:05:42 -0400

    Add npm run dev script

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const app = require('./app.js');
 const port = 4200;
 
 app.listen(port, () => {
-  console.log(`Glimpse is listening on ${port}`);
+  console.log(`Glimpse is listening on port ${port}`);
 });
